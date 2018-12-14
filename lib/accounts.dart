aeded4159bb2ce62a66a4d254e87df964f9b0f5b
Paul Nadolny <psnadolny@gmail.com>
2018-12-13 19:27:08 -0600

    Add url_launcher dependency and try it out when you tap a list tile.

--- lib/accounts.dart
+++ lib/accounts.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'models.dart';
-import 'styles.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AccountWidget extends StatelessWidget {
   final User user;
@@ -35,34 +35,26 @@ class AccountWidget extends StatelessWidget {
                         // Decode the JSON
                         var accounts = json.decode(snapshot.data.toString());
 
+                        //User.of(context).name;
+                        /*
+                            ScopedModelDescendant<User>(
+                              builder: (context, child, model) {
+                                return Text(model.name);
+                              },
+                            ),
+                            */
+
                         return new ListView.builder(
-                          // Build the ListView
                           itemBuilder: (BuildContext context, int index) {
-                            return GestureDetector(
-                                onTap: () =>
-                                    print('Hello' + accounts[index]['id']),
-                                child: Card(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.stretch,
-                                    children: <Widget>[
-                                      ScopedModelDescendant<User>(
-                                        builder: (context, child, model) {
-                                          return Text(model.name);
-                                        },
-                                      ),
-                                      Container(
-                                        child: Text(
-                                            // Read the name field value and set it in the Text widget
-                                            accounts[index]['name'],
-                                            // set some style to text
-                                            style: Styles.baseTextStyle),
-                                        // added padding
-                                        padding: const EdgeInsets.all(15.0),
-                                      )
-                                    ],
-                                  ),
-                                ));
+                            return new ListTile(
+                                title:  Text(accounts[index]['name']),
+                                subtitle: Text(accounts[index]['id']),
+                                onTap: () async {
+                                  final fakeUrl = "http://www.flutter.io";
+                                  if (await canLaunch(fakeUrl)) {
+                                    launch(fakeUrl);
+                                  }
+                                });
                           },
                           itemCount: accounts == null ? 0 : accounts.length,
                         );
